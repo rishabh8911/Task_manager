e9ce8b7e40369a36722b0e5da53a26b4271fec33
rishabh8911 <rsbh1125@gmail.com>
2024-10-05 13:18:52 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const taskForm = document.getElementById("task-form")
 const taskInput = document.getElementById("task-input")
-const addTask = document.getElementById("add-task")
+const taskList = document.getElementById("task-list")
 
 
 // create tasks to store tasks
@@ -13,4 +13,46 @@ function saveTasks(){
 
 }
 
-function save
+function renderTask(){
+    taskList.innerHTML='';
+    tasks.forEach((task,index)=>{
+        const li = document.createElement('li');
+        li.className = `task-item ${task.completed? 'completed': ''}`;
+        li.innerHTML=`
+                <span class="task-text">${task.text}</span>
+                <div>
+                    <button class="complete-btn"> ${task.completed ? 'Undo':'Complete'}</button>
+                    <button class ="delete-btn"> Delete</button>
+                </div>
+
+                 
+        `;
+
+        li.querySelector('.complete-btn').addEventListener('click',()=>{
+            tasks[index].completed = !tasks[index].completed;
+            saveTasks();
+            renderTask();
+        });
+
+        li.querySelector('.delete-btn').addEventListener('click',()=>{
+            tasks.splice(index,1);
+            saveTasks();
+            renderTask();
+        });
+        taskList.appendChild(li);
+    });
+}
+
+taskForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const taskText = taskInput.value.trim();
+    if(taskText){
+        tasks.push({text: taskText, completed:false});
+        saveTasks();
+        renderTask();
+        taskInput.value='';
+    }
+ 
+})
+
+renderTask();
